675c13c6745bd13e1ecc056dd00c5c3a0f6e4179
ildyo <ildyo@INATHI>
2024-05-28 08:56:41 +0200

    Add project files.

---
+++ Salary Calculations/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Salary_Calculations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            string s1 = "hello ";
+            string s2 = s1;
+            s1 += "world";
+           
+            System.Console.WriteLine(s2);
+        }
+    }
+}
+
+
